e5b5169f47df9fb4fe625fbe83909e6919eb9afa
zxcvb <zxcvb0528@gmail.com>
2026-04-25 22:02:53 +0900

    feat: add ALTCOIN_EMA_SCALPING to StrategyType

--- src/main/java/main/model/StrategyType.java
+++ src/main/java/main/model/StrategyType.java
@@ -12,5 +12,6 @@ public enum StrategyType {
     HEIKIN_ASHI_ICHIMOKU,
     SUPER_TREND,           // SuperTrend 전략
     DYNAMIC_EXIT_SCALPING,  // E0V1E_53_Sharpe 류 동적 손절/익절 스캘핑
-    BOLLINGER_BAND_REVERSION // BB 평균회귀 (현재 활성)
+    BOLLINGER_BAND_REVERSION, // BB 평균회귀
+    ALTCOIN_EMA_SCALPING      // 알트코인 EMA 크로스 스캘핑 (현재 활성)
 }
